7df28648217c20fb6f9c9e0731bbd03fadeb5df1
Hussain Ahmad <engrhusainahmad@gmail.com>
2022-01-22 02:23:57 +0500

    switching all products and fist five ones

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="flex justify-between">
-        <h1 className="mx-10 mt-3">All Products</h1>
+        <h1 className="mx-10 mt-3">{!showFive ? "All Products" : "First Five Products"}</h1>
         <button
           className="text-white px-3 py-2 mx-10 mt-3 rounded-lg bg-purple-700 hover:bg-purple-600"
           onClick={() => setShowFive(!showFive)}
